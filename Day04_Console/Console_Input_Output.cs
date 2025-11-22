6cd52e9fcb647fbecdb8e8eeb49aff0916dfe67c
Mobin <136748080+LightHawk6545@users.noreply.github.com>
2025-11-22 17:30:31 +0330

    Add files via upload

---
+++ Day04_Console/Console_Input_Output.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mobin_learning
+{
+    internal class Program
+    {
+        private static object coco;
+
+        static void Main(string[] args)
+        {
+            // This session is dedicated for practicing console input and output
+
+
+            Console.WriteLine("mobin: come here take the shopping list put two extra items if you want: ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Press Enter again and then add your desireable items ... ");
+            Console.ReadLine();
+
+
+            string item_1 = Console.ReadLine();
+            string item_2 = Console.ReadLine();
+            const int pin = 3342;
+            int Receipt_Code = 435376;
+
+            Console.WriteLine(" ");
+
+
+            Console.WriteLine("Okay so for the list you want me to get, " + item_1 + " first and then " + item_2 + " right?, leave it to me.");
+            Console.ReadLine();
+
+
+            Console.WriteLine("Oh also i could get your favorite snack, what was the name again? ");
+            Console.WriteLine("Enter your favorite snack's name... "); string Ask_Favorite = Console.ReadLine();
+            Console.WriteLine("Oh yes so it is... " + Ask_Favorite + " Sure i'll get it for you");
+            Console.ReadLine();
+
+
+            Console.WriteLine("Clerk: Hello mobin do you wanna pay for those items aswell? i'll put them for you in one list");
+            Console.ReadLine();
+            Console.WriteLine("Oh yes please");
+            Console.WriteLine("The items are " + item_1 + " " + item_2 + " " + Ask_Favorite + " That's all, it will be 59.99$");
+            Console.ReadLine();
+
+
+            Console.WriteLine("Please insert your pin and receipt code all at once");
+            Console.ReadLine();
+
+
+            Console.Write("Sure ");
+            Console.Write(pin);
+            Console.Write(", That is my pin, ");
+            Console.Write(Receipt_Code);
+            Console.WriteLine("And the following is my receipt code");
+
+
+            Console.WriteLine("Clerk: Thank you, please rate our service if you would like...");
+            Console.WriteLine("Enter your rating from 5 stars: ");
+            Console.ReadLine();
+
+            int rate = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your rating out of 5 was... " + rate + " stars. Thank you for helping us grow.");
+
+
+        }
+    }
+}
---
+++ Day04_Console/If_statements.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mobin_learning
+{
+    internal class Program
+    {
+        private static object coco;
+
+        static void Main(string[] args)
+        {
+            // this section is about practicing my exposure to "if" and conditional statements
+            //i'll practice using a guest story
+
+
+            int guests = 0;
+            double time = 5;
+
+            string greeting = "your welcome, we are more than glad to have you.";
+            string greeting_1 = "please make yourself comfortable, we have a seat right there";
+
+            Console.WriteLine("ALRIGHT MOBIN!!!, do we the guests here for dinner?");
+            Console.ReadLine();
+
+            Console.WriteLine("Not yet, but i can check for you!");
+            Console.ReadLine();
+
+            Console.WriteLine("Yes that is amazing, i'll count on you then, they should all be here at 8 pm");
+            Console.WriteLine();
+            Console.ReadLine();
+
+
+            if (guests <= 0 && time >= 5)
+            {
+                Console.WriteLine("mobin: no one seem to come so early at 5 pm"); Console.ReadLine();
+                Console.WriteLine("maybe i should come an hour later, i can do some preparation meanwhile.");
+                Console.ReadLine();
+                time++;
+            }
+
+
+            if (time == 6)
+            {
+                Console.WriteLine("An hour later..., time: " + time + "pm.");
+                Console.ReadLine();
+
+                Console.WriteLine("*Door bell rang*");
+                Console.ReadLine();
+
+                ++guests;
+
+                Console.WriteLine("mobin: OH we have " + guests + " guest by the door, i'll be opening");
+                Console.ReadLine();
+
+                Console.WriteLine(greeting + ", just about great timing, henry.");
+                ++time;
+            }
+
+
+            Console.WriteLine("so henry what kind of drink would you just tell me any number from 1 to 3, i'll serve you!");
+            Console.ReadLine();
+
+            Console.WriteLine("henry: thank you mobin let me see what you have, i like the diversity here");
+            Console.ReadLine();
+
+
+            Console.WriteLine("I choose:");
+            Console.WriteLine("*choose now");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine();
+
+            if (choice == 1)
+            {
+                Console.WriteLine("mobin: marvelous choice we have the best wines over here, please let me pour some, special for you.");
+                Console.ReadLine();
+
+            }
+
+
+            else if (choice == 2)
+            {
+                Console.WriteLine("Oh beer is a delight in this cold weather, right away!");
+                Console.ReadLine();
+            }
+
+
+            else if (choice == 3)
+            {
+                Console.WriteLine("sure i'll have you some cold water now");
+                Console.ReadLine();
+            }
+
+
+            else
+            {
+                Console.WriteLine("sorry, i dont think we have that right now.");
+                Console.ReadLine();
+
+                Console.WriteLine("would you like to reconsider choices? ");
+                Console.ReadLine();
+
+                Console.WriteLine("I'll come back later in case you're not sure yet.");
+                Console.ReadLine();
+
+            }
+
+
+            guests++;
+            Console.WriteLine("*Door bell rang again*");
+            Console.WriteLine("Finally our last guest is here");
+            Console.ReadLine();
+
+            if (guests == 2 && time >= 7)
+            {
+                Console.WriteLine("coco has arrived!");
+                Console.ReadLine();
+                Console.WriteLine("I'll serve dinner and ask both if they need anything");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
